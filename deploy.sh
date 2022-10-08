5ef38448caff0786156a63abeeebdcb49ff116e2
Andrés Esteban Rodríguez Jiménez <andrudeluxe@gmail.com>
2022-10-08 13:39:25 -0500

    gh pages config

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+set -e
+
+# compilado
+npm run build
+
+# navega al directorio de salida de compilación
+dist cd
+
+# si estás desplegando en un dominio personalizado
+# echo 'www.ejemplo.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# si estás desplegando en https://<NOMBRE DE USUARIO>.github.io
+# git push -f git@github.com:<NOMBRE DE USUARIO>/<NOMBRE DE USUARIO>.github.io.git main
+
+# si estás desplegando en https://<NOMBRE DE USUARIO>.github.io/<REPO>
+git push -f git@github.com:AndruTRADX/github-profile-mui-react.git main:gh-pages
+
+cd -
